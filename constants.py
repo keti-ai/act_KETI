680be1bdeaa6bc6989f55345a7ae3a8bb0b4b499
syh <syh20131292@gmail.com>
2023-11-01 16:05:02 +0900

    edit code for imitate move glue

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import pathlib
 
 ### Task parameters
-DATA_DIR = './data'
+DATA_DIR = '/media/syh/hdd/data/aloha_dataset'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
